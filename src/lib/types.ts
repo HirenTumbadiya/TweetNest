5cef5d59c92fbc5bcd98c98a65cd31c9cd400852
Hiren Tumbadiya <tumbadiyahiren@gmail.com>
2024-09-14 15:58:44 +0530

    added global notfound and loader, added user profile,count,postscreens

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -9,6 +9,8 @@ export function getUserDataSelect(loggedInUserId: string) {
     username: true,
     displayName: true,
     avatarUrl: true,
+    bio: true,
+    createdAt: true,
     followers: {
       where: {
         followerId: loggedInUserId,
@@ -19,11 +21,16 @@ export function getUserDataSelect(loggedInUserId: string) {
     },
     _count: {
       select: {
+        posts: true,
         followers: true,
       },
     },
   } satisfies Prisma.UserSelect;
 }
+
+export type UserData = Prisma.UserGetPayload<{
+  select: ReturnType<typeof getUserDataSelect>;
+}>;
 
 export function getPostDataInclude(loggedInUserId: string) {
   return {
