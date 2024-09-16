56857c837267ba3e18cebd15a4bea814010da77f
Hiren Tumbadiya <tumbadiyahiren@gmail.com>
2024-09-16 16:27:37 +0530

    implimented profile update functionality

---
+++ src/app/(main)/users/[username]/EditProfileDialog.tsx
@@ -0,0 +1,117 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { UserData } from "@/lib/types";
+import {
+  UpdateUserProfileValues,
+  uploadUserProfileSchema,
+} from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, useForm } from "react-hook-form";
+import { useUpdateProfileMutation } from "./mutation";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import LoadingButton from "@/components/LoadingButton";
+
+interface EditProfileDialogProps {
+  user: UserData;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export default function EditProfileDialog({
+  user,
+  open,
+  onOpenChange,
+}: EditProfileDialogProps) {
+  const form = useForm<UpdateUserProfileValues>({
+    resolver: zodResolver(uploadUserProfileSchema),
+    defaultValues: {
+      displayName: user.displayName,
+      bio: user.bio || "",
+    },
+  });
+
+  const mutation = useUpdateProfileMutation();
+
+  async function onSubmit(values: UpdateUserProfileValues) {
+    mutation.mutate(
+      {
+        values,
+      },
+      {
+        onSuccess: () => {
+          onOpenChange(false);
+        },
+      },
+    );
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Profile</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="displayName"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Display Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Your Display Name"
+                        type="text"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="bio"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Bio</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="About Your Self..."
+                        {...field}
+                        className="resize-none"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <DialogFooter>
+              <LoadingButton type="submit" loading={mutation.isPending}>
+                Save
+              </LoadingButton>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -49,6 +49,11 @@ export interface PostPage {
   nextCursor: string | null;
 }
 
+export interface PostsPage {
+  posts: PostData[];
+  nextCursor: string | null;
+}
+
 export interface FollowerInfo {
   followers: number;
   isFollowedByUser: boolean;
