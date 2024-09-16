5c73f91768f35035d77bf6ab943c515bc4ea759c
Hiren Tumbadiya <tumbadiyahiren@gmail.com>
2024-09-16 19:02:12 +0530

    updated crop image dialog box and updated image functionality

--- src/app/(main)/users/[username]/EditProfileDialog.tsx
+++ src/app/(main)/users/[username]/EditProfileDialog.tsx
@@ -11,9 +11,9 @@ import {
   uploadUserProfileSchema,
 } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, useForm } from "react-hook-form";
 import { useUpdateProfileMutation } from "./mutation";
 import {
+  Form,
   FormControl,
   FormField,
   FormItem,
@@ -23,6 +23,14 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import LoadingButton from "@/components/LoadingButton";
+import Image, { StaticImageData } from "next/image";
+import { useRef, useState } from "react";
+import { Label } from "@/components/ui/label";
+import avatarPlaceholder from "@/assets/ProfilePlaceholder.jpg";
+import { useForm } from "react-hook-form";
+import { CameraIcon } from "lucide-react";
+import CropImageDialog from "@/components/common/CropImageDialog";
+import Resizer from "react-image-file-resizer";
 
 interface EditProfileDialogProps {
   user: UserData;
@@ -45,6 +53,8 @@ export default function EditProfileDialog({
 
   const mutation = useUpdateProfileMutation();
 
+  const [croppedAvatar, setCroppedAvatar] = useState<Blob | null>(null);
+
   async function onSubmit(values: UpdateUserProfileValues) {
     mutation.mutate(
       {
@@ -64,6 +74,17 @@ export default function EditProfileDialog({
         <DialogHeader>
           <DialogTitle>Edit Profile</DialogTitle>
         </DialogHeader>
+        <div className="space-y-1.5">
+          <Label>Avatar</Label>
+          <AvatarInput
+            src={
+              croppedAvatar
+                ? URL.createObjectURL(croppedAvatar)
+                : avatarPlaceholder
+            }
+            onImageCropped={setCroppedAvatar}
+          />
+        </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
             <FormField
@@ -113,5 +134,72 @@ export default function EditProfileDialog({
         </Form>
       </DialogContent>
     </Dialog>
+  );
+}
+
+interface AvatarInputProps {
+  src: string | StaticImageData;
+  onImageCropped: (blob: Blob | null) => void;
+}
+
+function AvatarInput({ src, onImageCropped }: AvatarInputProps) {
+  const [imageToCrop, setImageToCrop] = useState<File>();
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  function onImageSelected(image: File | undefined) {
+    if (!image) return;
+
+    Resizer.imageFileResizer(
+      image,
+      1024,
+      1024,
+      "WEBP",
+      100,
+      0,
+      (uri) => setImageToCrop(uri as File),
+      "file",
+    );
+  }
+
+  return (
+    <>
+      <input
+        type="file"
+        accept="image/"
+        onChange={(e) => onImageSelected(e.target.files?.[0])}
+        ref={fileInputRef}
+        className="hidden sr-only"
+      />
+      <button
+        type="button"
+        onClick={() => fileInputRef.current?.click()}
+        className="group relative block"
+      >
+        <Image
+          src={src}
+          alt="Avatar Preview"
+          width={150}
+          height={150}
+          className="size-32 flex-none rounded-full object-cover"
+        />
+        <span className="absolute inset-0 m-auto flex size-12 items-center justify-center rounded-full bg-black bg-opacity-30 text-white transition-colors duration-200 group-hover:bg-opacity-25">
+          <CameraIcon size={24} />
+        </span>
+      </button>
+      {imageToCrop && (
+        <CropImageDialog
+          src={URL.createObjectURL(imageToCrop)}
+          cropAspectRatio={1}
+          onCropped={onImageCropped}
+          onClose={() => {
+            setImageToCrop(undefined);
+            if (fileInputRef.current) {
+              fileInputRef.current.value = "";
+            }
+          }}
+        />
+      )}
+    </>
   );
 }
