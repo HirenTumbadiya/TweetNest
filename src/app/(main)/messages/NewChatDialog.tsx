00242e56b6fb98b29a93b8c9b0cedbf9a6ce578a
Hiren Tumbadiya <tumbadiyahiren@gmail.com>
2024-09-24 14:48:15 +0530

    new chat dialog box implimented for custom new chat

--- src/app/(main)/messages/NewChatDialog.tsx
+++ src/app/(main)/messages/NewChatDialog.tsx
@@ -1,3 +1,5 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
 interface NewChatDialogProps {
   onOpenChange: (open: boolean) => void;
   onChatCreated: () => void;
@@ -6,4 +8,15 @@ interface NewChatDialogProps {
 export default function NewChatDialog({
   onOpenChange,
   onChatCreated,
-}: NewChatDialogProps) {}
+}: NewChatDialogProps) {
+
+  return(
+    <Dialog open onOpenChange={onOpenChange}>
+      <DialogContent className="bg-card p-0">
+        <DialogHeader className="px-6 pt-6">
+          <DialogTitle>New Chat</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  )
+}
